196a277d3d290b63174a358be1fd184546eb77cc
Rongon <rongonhasan2@.com>
2023-07-27 18:32:41 +0100

    calculation

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,31 @@
+import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+const Products = (props) => {
+   const { img, name, seller, ratings, price } = props.product;
+   const handelAddToCart = props.handelAddToCart;
+
+//     const handelAddToCart= (product) =>{
+//   console.log(product)
+//     }
+   
+
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+           <div className='product-info'>
+           <h6 className='product-name'>{name}</h6>
+            <p>Price: ${price}</p>
+            <p>Manufacturer: {seller}</p>
+             <p>Rating: {ratings} Star</p>
+           </div>
+           <button onClick={() => handelAddToCart(props.product)} className='btn-cart'>Add to Cart
+           <FontAwesomeIcon icon={faShoppingCart} />
+           </button>
+        </div>
+    );
+};
+
+export default Products;
+
---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,49 @@
+import  { useEffect, useState } from 'react';
+import "./Shop.css"
+import  Product from '../Product/Product';
+import Cart from '../../Cart/Cart';
+
+
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([])
+
+
+useEffect (() => {
+fetch("products.json")
+.then (res=>res.json())
+.then (data => setProducts(data))
+
+},[])
+
+
+const handelAddToCart= (product) =>{
+   const newCart= [...cart, product]
+   setCart(newCart);
+}
+
+    return (
+        <div className='shop-container'>
+
+            <div className="products-container">
+{
+    products.map(product => <Product
+    key={product.id}
+    product={product}
+    handelAddToCart={handelAddToCart}
+    ></Product>)
+}
+
+
+            </div>
+
+            <div className="card-container">
+                <Cart cart={cart}> </Cart>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Shop;
