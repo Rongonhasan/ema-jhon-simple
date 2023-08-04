3f4f97e8495e1520f2f4c1762ed47621165be189
Rongon <rongonhasan2@.com>
2023-08-04 11:18:42 +0100

    card done

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,31 +1,29 @@
-import './Product.css';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css';
 
-const Products = (props) => {
-   const { img, name, seller, ratings, price } = props.product;
-   const handelAddToCart = props.handelAddToCart;
+const Product = (props) => {
 
-//     const handelAddToCart= (product) =>{
-//   console.log(product)
-//     }
-   
+    const { img, name, seller, ratings, price } = props.product;
+    const handleAddToCart = props.handleAddToCart;
+
 
     return (
         <div className='product'>
             <img src={img} alt="" />
-           <div className='product-info'>
-           <h6 className='product-name'>{name}</h6>
-            <p>Price: ${price}</p>
-            <p>Manufacturer: {seller}</p>
-             <p>Rating: {ratings} Star</p>
-           </div>
-           <button onClick={() => handelAddToCart(props.product)} className='btn-cart'>Add to Cart
-           <FontAwesomeIcon icon={faShoppingCart} />
-           </button>
+            <div className='product-info'>
+                <h6 className='product-name'>{name}</h6>
+                <p>Price: ${price}</p>
+                <p>Manufacturer: {seller}</p>
+                <p>Rating: {ratings} Stars</p>
+            </div>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>
+                Add to Cart
+                <FontAwesomeIcon icon={faShoppingCart} />
+                </button>
         </div>
     );
 };
 
-export default Products;
-
+export default Product;
